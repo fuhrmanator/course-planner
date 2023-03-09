3d2611889d3813044edf26e8db663c9a5c357462
Nicolas Plourde <65302751+ZebuleETS@users.noreply.github.com>
2023-03-09 12:11:40 -0500

    URL change

--- src/components/util/fetchCourseICAL.tsx
+++ src/components/util/fetchCourseICAL.tsx
@@ -7,7 +7,19 @@ const fetchCourseICAL = async (code: string, group:number, year:number, semester
     url.searchParams.set('Groupe', group < 9 ? "0"+group : ""+group);
     url.searchParams.set('Session', year+""+semester);
 
+    const config = {
+        headers: {
+            'Content-Type': 'text/calendar',
+            'Accept': 'text/calendar',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        }
+    }
+
     const finalUrl = `/api/proxy?url=${encodeURIComponent(url.href)}`;
+    //const data = await axios.get(url.href, config).catch((err: any) => console.log(err));
+    //return data.data.toString();
     const data  = await fetch(finalUrl);
     const textData = await data.text();
     return textData;
