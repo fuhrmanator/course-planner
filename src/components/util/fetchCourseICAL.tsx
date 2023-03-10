df8a77fb10d31bda7307f5b23aaf195f39f945cb
Nicolas Plourde <65302751+ZebuleETS@users.noreply.github.com>
2023-03-10 10:06:46 -0500

    remove axios

--- src/components/util/fetchCourseICAL.tsx
+++ src/components/util/fetchCourseICAL.tsx
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const ressourceURL = 'https://portail.etsmtl.ca/ICal/SeancesCours?';
 
 const fetchCourseICAL = async (code: string, group:number, year:number, semester:number): Promise<string> => {
@@ -7,22 +6,10 @@ const fetchCourseICAL = async (code: string, group:number, year:number, semester
     url.searchParams.set('Groupe', group < 9 ? "0"+group : ""+group);
     url.searchParams.set('Session', year+""+semester);
 
-    const config = {
-        headers: {
-            'Content-Type': 'text/calendar',
-            'Accept': 'text/calendar',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-        }
-    }
-
-    //const finalUrl = `/api/proxy?url=${encodeURIComponent(url.href)}`;
-    const data = await axios.get(url.href, config).catch((err: any) => console.log(err));
-    return data.data.toString();
-    //const data  = await fetch(finalUrl);
-    //const textData = await data.text();
-    //return textData;
+    const finalUrl = `/api/proxy?url=${encodeURIComponent(url.href)}`;
+    const data  = await fetch(finalUrl);
+    const textData = await data.text();
+    return textData;
 
 }
 
